1e4ec2abe6e283502f43ac0fa5e4ad32062774eb
David Y <78624155+dredavidOps@users.noreply.github.com>
2024-07-01 05:07:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'figure-frittuese-casking'
-    SQLALCHEMY_DATABASE_URI = 'pymysql://kindabyt_admin:Addo-Dee-4-Commot@jdbc:mysql://localhost:3306/kindabyt'
+    SQLALCHEMY_DATABASE_URI = 'pymysql+mysql://localhost:3306/kindabyt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'pymysql://kindabyt_admin:Addo-Dee-4-Commot@jdbc:mysql://localhost:3306/kindabyt'
+    SQLALCHEMY_DATABASE_URI = 'pymysql+mysql://localhost:3306/kindabyt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
